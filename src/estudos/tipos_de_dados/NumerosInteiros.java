68a34ad87e8f81fd0361ee24df8aaf15d7b688b4
tallyto.rodrigues <tallyto.rodrigues@senior.com.br>
2022-07-19 20:58:22 -0300

    Numeros inteiros e reais

---
+++ src/estudos/tipos_de_dados/NumerosInteiros.java
@@ -0,0 +1,20 @@
+package estudos.tipos_de_dados;
+
+public class NumerosInteiros {
+   public static void main(String[] args) {
+     // tipos primarios/primitivos
+     int num = 4; // inteiro
+     short num2 = 4; // inteiro (curto/menor/baixo)
+
+     // tipos nao primarios
+     Integer num3 = 98;
+     Short num4 = 99;
+
+     System.out.println("int -> "+ num);
+     System.out.println("short -> "+ num2);
+     System.out.println("Integer -> "+ num3);
+     System.out.println("Short -> "+ num4);
+   }
+
+
+}
---
+++ src/estudos/tipos_de_dados/NumerosReais.java
@@ -0,0 +1,17 @@
+package estudos.tipos_de_dados;
+
+public class NumerosReais {
+    public static void main(String[] args) {
+        // Por padrao, os numeros reais em Java sao considerados double
+        float preco = 23.4f;
+        double preco2 = 24.4;
+
+        Float preco3 = 44.5f;
+        Double preco4 = 44.5;
+
+        System.out.println("float -> " + preco);
+        System.out.println("double -> " + preco2);
+        System.out.println("Float -> " + preco3);
+        System.out.println("Double -> " + preco4);
+    }
+}
